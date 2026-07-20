3251a6170b3ac1c5a03ffd0536e8c8843dd594aa
Bryanna-21 <bryannamanu@gmail.com>
2026-07-20 19:01:46 +0300

    Create App.js

---
+++ academia-ai/frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Academia AI</h1>
+      <p>Welcome, Sir. The tutor platform is initializing.</p>
+      {/* Future: Login, Class Dashboard, Screen Share */}
+    </div>
+  );
+}
+
+export default App;
